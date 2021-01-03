be09f070f455fef33ad1b0eead192c3b32884c9f
repl.it user <replituser@example.com>
2021-01-03 07:26:54 +0000

    210103 최대 제한은 두되, 최소거리 제한은 안두는 방향으로 코딩해봐야겠다.

--- BinarySearch/boj_2110/Main.java
+++ BinarySearch/boj_2110/Main.java
@@ -10,10 +10,58 @@ public class Main {
 		Arrays.sort(housePos);
 		int minPos = housePos[0];
 		int maxPos = housePos[numOfHouse-1];
-		int properDist = (maxPos - minPos) / numOfWifi;
+		int left = 1;
+		int right = (maxPos - minPos) / (numOfWifi-1) + 1;
+		int mid;
+		int ret = 1000000001;
 
-		// dist 이상인 처음의 위치를 선택한다.
+		while(left <= right){
+			mid = (left + right + 1) / 2; // 점프해볼 거리
 
+			int closestDist = jump(mid);
+
+			// 공유기가 부족한 경우 점프거리를 줄여야함
+			if(closestDist == -1){
+				right = mid - 1;
+			}
+			// 공유기가 남는 경우 점프거리를 늘려야함. 
+			else if(closestDist == -2){
+				left = right + 1;
+			}
+			// 공유기 개수가 딱 맞는 경우 현재의 최인접 최대거리를 저장하고 점프거리를 늘려봄.
+			else {
+				ret = Math.min(ret, closestDist);
+				left = right + 1;
+			}
+		}
+
+		return ret;
+
+	}
+
+	static int jump(int dist){
+		int pos = housePos[0];
+		int lastPos = housePos[0];
+		int closestDist = 1000000001;
+		int wifiCnt = 0;
+		for(int i=0; i<numOfHouse; i++){
+			if(housePos[i] >= pos){
+				pos = housePos[i] + dist;
+				wifiCnt++;
+				if(i != 0){
+					closestDist = Math.min(closestDist, housePos[i] - lastPos);
+					lastPos = housePos[i];
+				}
+			}
+		}
+
+		if(wifiCnt > numOfWifi){
+			closestDist = -1;
+		}
+		else if(wifiCnt < numOfWifi){
+			closestDist = -2;
+		}
+		return closestDist;
 	}
 
 	public static void main(String[] args) throws IOException{
@@ -21,7 +69,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		numOfHouse = Integer.parseInt(st.nextToken());
-		numOfWifi = Integer.parseInt(st.nextToken);
+		numOfWifi = Integer.parseInt(st.nextToken());
 
 		housePos = new int[numOfHouse];
 		for(int i=0; i<numOfHouse; i++){
